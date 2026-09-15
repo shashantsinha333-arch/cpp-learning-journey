908ede0c26816a7fe8acdf9ab40d9a8b1c9b7061
Shashant sinha <shashantsinha333@gmail.com>
2026-09-15 20:14:52 +0530

    Start C++ learning journey

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    string name;
+    int age;
+    string city;
+
+    cout<<"Enter your name:";
+    cin>>name;
+
+    cout<<"Enter your age:";
+    cin>>age;
+
+    cout<<"Enter your city:";
+    cin>>city;
+
+    cout<<"My name is "<<name<<" I am "<<age<<" years old. "<<" I live in"<<city;
+}
