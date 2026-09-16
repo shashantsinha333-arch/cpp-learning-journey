01267d2c4acf1d70f8eb35065709530a8ee0e3c0
Shashant sinha <shashantsinha333@gmail.com>
2026-09-16 14:55:45 +0530

    Complete data types practice

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main(){
-    
     string name;
     int age;
-    string city;
+    float height;
+    double weight;
+    char firstletter;
 
     cout<<"Enter your name:";
     cin>>name;
@@ -13,8 +16,23 @@ int main(){
     cout<<"Enter your age:";
     cin>>age;
 
-    cout<<"Enter your city:";
-    cin>>city;
+    cout<<"Enter your height";
+    cin>>height;
 
-    cout<<"My name is "<<name<<" I am "<<age<<" years old. "<<" I live in"<<city;
+    cout<<"Enter your weight:";
+    cin>>weight;
+
+    cout<<"Enter first letter of your city:";
+    cin>>firstletter;
+
+    int sum = age + 1;
+
+
+    cout<<"Your name is "<<name<<"\n";
+    cout<<" You are "<<age<<" years old "<<"\n";
+    cout<<" Your height is "<<height<<"\n";
+    cout<<" Your weight is "<<weight<<"\n";
+    cout<<" First letter of your city is "<<firstletter<<"\n";
+    cout<<" After 1 year you will "<<sum<<" years old ";
+    return 0;
 }
