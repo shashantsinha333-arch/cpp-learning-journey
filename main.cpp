381b260a440987ec1022fa766532e4d120fb5d66
Shashant sinha <shashantsinha333@gmail.com>
2026-09-20 02:17:24 +0530

    Student Result & Course Grade Checker

--- main.cpp
+++ main.cpp
@@ -2,44 +2,129 @@
 using namespace std;
 
 int main(){
+    
+    string name;
+    string status;
+    int age;
+    double percentage;
+    char grade;
 
-    int day;
+    cout<<"Enter your name:";
+    cin>>name;
 
-    cout<<"Enter your day:";
-    cin>>day;
+    cout<<"Enter your age:";
+    cin>>age;
 
-    switch (day){
+    cout<<"Enter your percentage:";
+    cin>>percentage;
+
+    cout<<"Enter your grade:";
+    cin>>grade;
+
+    if (age >= 17){
+
+        if(percentage >= 60 ){
+            cout<<"Eligible."<<"\n";
+            status = "Eligible";
+
+        }
+        else if (percentage < 60){
+            cout<<"Not eligible due to percentage."<<"\n";
+            status = "Not eligible-percentage";
+         }
+        
+         }
+
+        else{
+            cout<<"Not eligible due to age."<<"\n";
+            status = "Not eligible-age";
+        }
+
+        switch (grade){
+
+            case 'A':
+            cout<<"excellent";
+            break;
+
+            case 'B':
+            cout<<"Very good";
+            break;
+
+            case 'C':
+            cout<<"Good";
+            break;
+
+            case 'D':
+            cout<<"Average";
+            break;
+
+            case 'F':
+            cout<<"Fail";
+            break;
+
+            default:
+            cout<<"Invalid grade";
+        }
+    
+        if (percentage >= 60 && grade != 'F'){
+            cout<<"Passed."<<"\n";
+        }
+        else{
+            cout<<"Need improvment."<<"\n";
+        }
+
+        int choice;
+        cout<<"Enter your choice:";
+        cin>>choice;
+
+        switch (choice){
 
         case 1:
-        cout<<"Monday";
+        cout<<name<<"\n";
+        cout<<age<<"\n";
+        cout<<percentage<<"\n";
+        cout<<grade<<"\n";
         break;
 
         case 2:
-        cout<<"Tuesday";
+        cout<<status;
         break;
 
         case 3:
-        cout<<"Wednesday";
+        switch (grade){
+         
+            case 'A':
+            cout<<"excellent";
+            break;
+
+            case 'B':
+            cout<<"Very good";
+            break;
+
+            case 'C':
+            cout<<"Good";
+            break;
+
+            case 'D':
+            cout<<"Average";
+            break;
+
+            case 'F':
+            cout<<"Fail";
+            break;
+
+            default:
+            cout<<"Invalid grade";
+        }
         break;
 
         case 4:
-        cout<<"Thursday";
-        break;
-
-        case 5:
-        cout<<"Friday";
-        break;
-
-        case 6:
-        cout<<"Staurday";
-        break;
-
-        case 7:
-        cout<<"Sunday";
+        cout<<"Exit";
         break;
 
         default:
-        cout<<"Invalid day";
+        cout<<"Invalid choice.";
+        
     }
 
     return 0;
